1f3527e73ccc931ba0a4777a3f0b963ce0103279
msleprosy <msleprosy@yandex.ru>
2019-06-28 12:54:03 +0300

    Adds tests for CloudPipelineAPIClient

--- elasticsearch-agent/src/test/java/com/epam/pipeline/elasticsearchagent/service/impl/CloudPipelineAPIClientTest.java
+++ elasticsearch-agent/src/test/java/com/epam/pipeline/elasticsearchagent/service/impl/CloudPipelineAPIClientTest.java
@@ -1,11 +1,15 @@
 package com.epam.pipeline.elasticsearchagent.service.impl;
 
 import com.epam.pipeline.elasticsearchagent.model.PipelineRunWithLog;
+import com.epam.pipeline.elasticsearchagent.service.impl.converter.storage.DataStorageLoader;
 import com.epam.pipeline.entity.datastorage.*;
-import com.epam.pipeline.entity.pipeline.Folder;
-import com.epam.pipeline.entity.pipeline.PipelineRun;
+import com.epam.pipeline.entity.metadata.MetadataEntry;
+import com.epam.pipeline.entity.pipeline.*;
+import com.epam.pipeline.entity.region.AbstractCloudRegion;
+import com.epam.pipeline.entity.region.AwsRegion;
 import com.epam.pipeline.entity.security.acl.AclClass;
 import com.epam.pipeline.entity.user.PipelineUser;
+import com.epam.pipeline.vo.EntityVO;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
@@ -20,6 +24,7 @@ import java.util.List;
 import java.util.Map;
 
 import static com.epam.pipeline.elasticsearchagent.TestConstants.USER;
+import static com.epam.pipeline.elasticsearchagent.TestConstants.USER_NAME;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -153,6 +158,12 @@ class CloudPipelineAPIClientTest {
 
     @Test
     void loadUserByName() {
+        when(cloudPipelineAPIClient.loadUserByName(USER_NAME))
+                .thenReturn(USER);
+        cloudPipelineAPIClient.loadUserByName(USER_NAME);
+        assertEquals(USER, USER);
+        verify(cloudPipelineAPIClient, atLeastOnce())
+                .loadUserByName(USER_NAME);
     }
 
     @Test
@@ -161,26 +172,63 @@ class CloudPipelineAPIClientTest {
 
     @Test
     void loadRegion() {
+        AbstractCloudRegion cloudRegion = new AwsRegion();
+        when(cloudPipelineAPIClient.loadRegion(1L)).thenReturn(cloudRegion);
+        cloudPipelineAPIClient.loadRegion(1L);
+        assertEquals(cloudRegion, cloudRegion);
+        verify(cloudPipelineAPIClient, atLeastOnce()).loadRegion(1L);
     }
 
     @Test
     void loadTool() {
+        Tool expectedTool = new Tool();
+        String expectedToolId = "toolId";
+        when(cloudPipelineAPIClient.loadTool(expectedToolId)).thenReturn(expectedTool);
+        cloudPipelineAPIClient.loadTool(expectedToolId);
+        assertEquals(expectedTool, expectedTool);
+        verify(cloudPipelineAPIClient, atLeastOnce()).loadTool(expectedToolId);
     }
 
     @Test
     void loadPipelineFolder() {
+        Folder expectedFolder = new Folder();
+        when(cloudPipelineAPIClient.loadPipelineFolder(1L)).thenReturn(expectedFolder);
+        cloudPipelineAPIClient.loadPipelineFolder(1L);
+        assertEquals(expectedFolder, expectedFolder);
+        verify(cloudPipelineAPIClient, atLeastOnce()).loadPipelineFolder(1L);
     }
 
     @Test
     void loadMetadataEntry() {
+        EntityVO expectedEntityVO = new EntityVO();
+        List<EntityVO> expectedEntityVOList = new ArrayList<>();
+        expectedEntityVOList.add(expectedEntityVO);
+        MetadataEntry expectedMetadataEntry = new MetadataEntry();
+        List<MetadataEntry> expectedMetadataEntryList = new ArrayList<>();
+        expectedMetadataEntryList.add(expectedMetadataEntry);
+        when(cloudPipelineAPIClient.loadMetadataEntry(expectedEntityVOList)).thenReturn(expectedMetadataEntryList);
+        cloudPipelineAPIClient.loadMetadataEntry(expectedEntityVOList);
+        assertEquals(expectedMetadataEntryList, expectedMetadataEntryList);
+        verify(cloudPipelineAPIClient, atLeastOnce()).loadMetadataEntry(expectedEntityVOList);
     }
 
     @Test
     void loadToolGroup() {
+        String expectedtoolGroupId = "toolGroupId";
+        ToolGroup expectedToolGroup = new ToolGroup();
+        when(cloudPipelineAPIClient.loadToolGroup(expectedtoolGroupId)).thenReturn(expectedToolGroup);
+        cloudPipelineAPIClient.loadToolGroup(expectedtoolGroupId);
+        assertEquals(expectedToolGroup, expectedToolGroup);
+        verify(cloudPipelineAPIClient, atLeastOnce()).loadToolGroup(expectedtoolGroupId);
     }
 
     @Test
     void loadDockerRegistry() {
+        DockerRegistry expectedDockerRegistry = new DockerRegistry();
+        when(cloudPipelineAPIClient.loadDockerRegistry(1L)).thenReturn(expectedDockerRegistry);
+        cloudPipelineAPIClient.loadDockerRegistry(1L);
+        assertEquals(expectedDockerRegistry, expectedDockerRegistry);
+        verify(cloudPipelineAPIClient, atLeastOnce()).loadDockerRegistry(1L);
     }
 
     @Test
